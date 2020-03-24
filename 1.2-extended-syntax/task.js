8a94ee692580fa428279983ecd9171ea8ef8b3cd
Alexey Yakushin <alex.aka.ion@gmail.com>
2020-03-24 20:08:18 +0300

    решение задания 1.2

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,14 +1,46 @@
-function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+function getResult(a, b, c) {
+    "use strict";
+
+    let result = [];
+
+    let D = Math.pow(b, 2) - 4 * a * c;
+
+    if (D > 0) {
+        result = [(-1 * b + Math.sqrt(D)) / (2 * a), (-1 * b - Math.sqrt(D)) / (2 * a)];
+    } else if (D === 0) {
+        result = [-1 * b / (2 * a)];
+    }
+
+    return result;
 }
 
-function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
+function getAverageMark(marks) {
+    let averageMark;
+
+    if (marks.length === 0) {
+        averageMark = 0;
+    } else {
+        if (marks.length > 5) {
+            console.log('Оценок в массиве > 5');
+            marks = marks.slice(0, 5);
+        }
+        averageMark = marks.reduce((a, c) => a + c) / marks.length;
+    }
+
+    return averageMark;
 }
 
-function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    // return result;
+function askDrink(name, dateOfBirthday) {
+
+    let year = dateOfBirthday.getFullYear();
+    let nowyear = new Date().getFullYear();
+    let result;
+
+    if (nowyear - year >= 18) {
+        result = `Не желаете ли олд-фэшн, ${name}?`;
+    } else {
+        result = `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
+    }
+
+    return result;
 }
